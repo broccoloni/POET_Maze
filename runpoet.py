5542644df9adb9d9bafccacef5f85ccfefe7f311
Amanjot Bhullar <bhullara@gra-login3.graham.sharcnet>
2021-12-16 17:49:17 -0500

    uploading to move running progress over to other computer

--- runpoet.py
+++ runpoet.py
@@ -1,5 +1,6 @@
 from POET import *
 import sys
+import timeit
 almightyint = 6671111 #call pizza pizza hey hey hey
 
 if __name__ == "__main__":
@@ -8,11 +9,16 @@ if __name__ == "__main__":
     numreps = int(sys.argv[3])
     seed = almightyint + 123456 * v
     print("v:\t{},eps:\t{},numreps:\t{}".format(v,eps,numreps))
+    start = timeit.default_timer()
+    savefreq = 5
     for i in range(500):
+        runningtime = timeit.default_timer() - start
+        if runningtime > 9*60*60:
+            savefreq = 1
         savefreq = 100000 #since were saving here anyways
         poet = POET(v,seed = seed,eps = eps,numreps = numreps,savefreq = savefreq,verbose = True)
         poet.loadlastsave()
-        poet.run(20)
+        poet.run(savefreq)
         poet.save()
         del poet
 
